361c0c3e400ca391330d87e050f2297cd95dc5f7
Miguel Bernabeu <mukik182@gmail.com>
2011-10-18 02:46:55 +0200

    C++: Add doxygen docs, code comments and README

--- unit02/c++/README.markdown
+++ unit02/c++/README.markdown
@@ -1,11 +1,54 @@
 C++ Implementation - Miguel Bernabeu Diaz < miguel.bernadi[AT]gmail[DOT]com >
+=============================================================================
+
+Last modified: 2011/10/18
 
 This project is based on the 2011 edition of "Stanford Online Introduction to Artificial Intelligence" (www.ai-class.com).
-The code of the example.cpp file is Public Domain. All other code is licensed under MIT License (http://www.opensource.org/licenses/mit-license.php).
+The code of file 'example.cpp' is released in the Public Domain. All other code is licensed under MIT License (http://www.opensource.org/licenses/mit-license.php).
 
-Algorithms:
+README TOC
+----------
+
+ * Algorithms
+ * Examples
+ * Known Problems
+ * BUGS
+ * TODO
+
+Algorithms
+----------
+
+All algorithms are basically the same, the difference is how they determine the best next path. For that purpose the algorithms are described simply as value compare functions. The goal of those value compare functions is to return the smallest positive value for the best option.
+
+All algorithm headers are stored in folder algorithm/ .
+
  * Breadth First Search  (BFS) => Implemented
  * Uniform Cost Search   (UCS)
  * Cheapest First Search (CFS)	
  * Depth First Search    (DFS)
  * A\* Search            (ASS)
+
+Examples
+--------
+
+You can find an usage example of the code in the file 'example.cpp'. The headers are commented with Doxygen (http://www.stack.nl/~dimitri/doxygen/index.html) comments.
+
+Known Problems
+--------------
+
+There's no known Problems right now.
+
+BUGS
+----
+
+If you find any bugs, please contact me at the email addressed above or post an issue at https://github.com/miguelbernadi/ai-class/issues . 
+
+TODO
+----
+
+ * Fix Algorithm usage via template parameter (actually disabled)
+ * Implement all listed algorithms
+ * Rationalize interfaces (adders/removers, setters/getters, const-correctness, references, data exposure...)
+ * Document whole code
+ * C++0x Features? (optional by compile flag?)
+
--- unit02/c++/TreeSearch.h
+++ unit02/c++/TreeSearch.h
@@ -28,20 +28,57 @@
 #include "Tree.h"
 #include "algorithms/Algorithms.h"
 
+/**
+ * Proxy class for structuring the search. Embeds the algorithm passed as template parameter.
+ *
+ * @note Algorithm template parameter is disabled right now.
+ */
 template <typename TreeType, typename SearchAlgorithm/* = BreadthFirstSearch<TreeType>*/ >
 class TreeSearch
 {
   public:
+   /**
+    * Renaming a list of Paths.
+    *
+    * @todo Move to Path class.
+    */
     typedef std::list< Path<TreeType>* > PathList;
+   /**
+    * Renaming of the data type stored in each node which must match the one used to define if the 
+    * goal has been reached.
+    */
     typedef typename TreeType::value_type::value_type GoalType;
 
-
+   /**
+    * List of Paths reaching the frontier. Used by all algorithms.
+    *
+    * @todo Move into algorithms, not needed anywhere else.
+    */
     PathList _frontier;
+   /**
+    * Set of Nodes already explored. Used by all algorithms.
+    *
+    * @todo Move into algorithms, not needed anywhere else.
+    */
     std::set<typename TreeType::value_type*> _explored;
+   /**
+    * Pointer to the Root Node of the Tree to be Searched.
+    *
+    * @note Redundant? Replaceable by Tree* and its memeber getRootPtr(). Searching a subtree would
+    * require defining a Tree object containing only the subtree.
+    */
     typename TreeType::value_type* TreeRoot;
-    PathList LPath;
 
+   /**
+    * Constructor.
+    */
     TreeSearch(typename TreeType::value_type* mNode) : TreeRoot( mNode ) { _frontier.push_back( new Path<TreeType>(TreeRoot) ); }
+   /**
+    * Function for applying the algorithm to the Tree. 
+    *
+    * @param The goal we want to achieve.
+    * @returns The best path according to the algorithm used.
+    */
     Path<TreeType>* find(GoalType Goal)
     {
     /*
@@ -59,39 +96,53 @@ class TreeSearch
                 add [path + a -> Result(s, a)] to frontier
      */
 
+    // Reuse variables instead of creating and destroying in the loop.
         Path<TreeType>* path;
         typename TreeType::value_type* Node;
+
 // Tries using SearchAlgorithm as holder of the actual algorithm implementation.
 //        SearchAlgorithm sa();
 //        return sa.find(Goal);
+
+    // Loop
         while(true)
         {
+        // Sanity check
             if(_frontier.empty()) throw( std::exception() );
+        // Get the shortest path on the frontier.
             path = _frontier.front();
+        // Remove the path from the frontier.
             _frontier.remove(path);
+        // Add the last Node of the path (Frontier Node) to the explored Nodes.
             Node = path->_path.back();
             _explored.insert(Node);
+        // Check if the present Node is our goal.
             if (Node->Data == Goal) return path;
 
-       // Add the checked Nodes children to a copy of the present path
-       // and add this path to _frontier.
+       // If 0 Node is a leaf Node and shouldn't be looked further.
             if( Node->has_children() != 0 )
             {
-                 for(int i = 0; i < Node->child_number; ++i)
-                 {
-                // Check if the child to be attached has been explored already
-                     if( _explored.find(Node->child[i]) == _explored.end() )
-                     {
-                         Path<TreeType>* newpath = new Path<TreeType>(*path);
-                         newpath->_path.push_back(Node->child[i]);
-                         _frontier.push_back(newpath);
-                     }
-                 }
+            // For all children.
+                for(int i = 0; i < Node->child_number; ++i)
+                {
+                // Check if the child to be attached has been explored already.
+                // Attach only the non-explored yet.
+                    if( _explored.find(Node->child[i]) == _explored.end() )
+                    {
+                    // Add the checked Nodes children to a copy of the present path
+                    // and add this path to _frontier. If 0 is a leaf Node.
+                        Path<TreeType>* newpath = new Path<TreeType>(*path);
+                        newpath->_path.push_back(Node->child[i]);
+                        _frontier.push_back(newpath);
+                    }
+                }
+            // Sort the _frontier list so the shortest paths are at the beginning
+            // of the list.
                 _frontier.sort();     
             }
         }
-    }
-};
+    }// End of find()
+};// End of TreeSearch
 
 #endif
 
